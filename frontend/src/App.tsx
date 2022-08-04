82dfa28de39d5893e6d6719320e3326beb941482
Mark Colby <macolby14@gmail.com>
2022-08-03 21:45:37 -0400

    fix - change app to div not body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,14 @@ import { UserWindow } from './components/UserWindow';
 
 function App() {
   return (
-    <body className="App">
+    <div className="App">
       <div className="ChatWindowContainer">
         <ChatWindow />
       </div>
       <div className="UserWindowContainer">
         <UserWindow />
       </div>
-    </body>
+    </div>
   );
 }
 
